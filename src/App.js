c4f8364796db5cf7f75410bcdf1dfdac60426dfc
ffuturesenior <shamray.ruslan068@gmail.com>
2023-06-07 15:08:45 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Main } from './modules/main';
 import { DetailsList, SelectionMode, Stack, Checkbox, DefaultPalette } from '@fluentui/react';
-import { DetailsListCustomColumnsExample } from './z';
+//import { DetailsListCustomColumnsExample } from './z';
 
 
 function App() {
